2e012f2d4c3f10fbd410e173bdee3e28cd7f17c9
americadabeiba <Dabeiba.Olazo@fundacion-jala.org>
2023-09-08 22:42:13 -0400

    Initial commit

---
+++ src/models/laboratorio.js
@@ -0,0 +1,11 @@
+class Laboratorio {
+    constructor(cod_lab, nombre_lab, capacidad, contador_maquinas, caracteristica, cod_campus) {
+        this.cod_lab = cod_lab;
+        this.nombre_lab = nombre_lab;
+        this.capacidad = capacidad;
+        this.contador_maquinas = contador_maquinas;
+        this.caracteristica = caracteristica;
+        this.cod_campus = cod_campus;
+        this.enlace = null;
+    }
+}
